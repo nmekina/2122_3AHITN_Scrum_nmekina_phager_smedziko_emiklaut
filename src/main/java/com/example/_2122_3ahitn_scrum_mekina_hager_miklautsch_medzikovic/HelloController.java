2dc3a6c5ed4c7ae5892326c339ca2afddca3ab90
nmekina <95867724+nmekina@users.noreply.github.com>
2022-05-05 09:02:17 +0200

    Initial commit

---
+++ src/main/java/com/example/_2122_3ahitn_scrum_mekina_hager_miklautsch_medzikovic/HelloController.java
@@ -0,0 +1,14 @@
+package com.example._2122_3ahitn_scrum_mekina_hager_miklautsch_medzikovic;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example._2122_3ahitn_scrum_mekina_hager_miklautsch_medzikovic {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example._2122_3ahitn_scrum_mekina_hager_miklautsch_medzikovic to javafx.fxml;
+    exports com.example._2122_3ahitn_scrum_mekina_hager_miklautsch_medzikovic;
+}

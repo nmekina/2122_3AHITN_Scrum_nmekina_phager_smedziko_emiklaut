33cfd6b74b1cedd9d52d97e7995c327605a3a5c1
Philipp04 <phager@htl-steyr.ac.at>
2022-05-19 14:58:16 +0200

    Spielfeld

--- src/main/java/com/example/Controller/HelloApplication.java
+++ src/main/java/com/example/Controller/HelloApplication.java
@@ -1,18 +1,34 @@
 package com.example.Controller;
 
+import Model.Player;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import static java.lang.Thread.sleep;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startmenue.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Startmenü");
+
+        scene.setOnKeyPressed(e-> {
+            System.out.println(e.getCode().toString());
+
+            if(!Player.getPressed() || Objects.equals(Player.getMovement(), "S")) {
+                Player.setMovement(e.getCode().toString());
+                Player.setPressed(true);
+            }else {
+                System.out.println("Already jumping");
+            }
+        });
+
+        stage.setTitle("Jump And Run");
         stage.setScene(scene);
         stage.show();
     }

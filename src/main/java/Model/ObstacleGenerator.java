98879b05d6664fa9910804a0875309d3f8cbb751
Philipp04 <phager@htl-steyr.ac.at>
2022-05-19 13:29:05 +0200

    ObstacleGenerator und Kollision

---
+++ src/main/java/Model/ObstacleGenerator.java
@@ -0,0 +1,189 @@
+package Model;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import static java.lang.Thread.sleep;
+
+public class ObstacleGenerator implements Runnable {
+    AnchorPane ap;
+    int big = 72;
+    int small = 28;
+    int x = 776;
+    int y = 237;
+    Rectangle activeObstacle;
+    int difficulty = 10;
+    static boolean beaten = false;
+    static boolean damage = false;
+    boolean stop = false;
+    int time;
+    AnimationTimer playerJump;
+    static Circle[] hearts = new Circle[3];
+    Score s = new Score();
+
+    AnimationTimer enemies = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+
+
+            if (activeObstacle.getX() > -36) {
+
+                    if (activeObstacle.getX() < 120 && activeObstacle.getX() > 30 && !beaten && !damage) {
+                        if (Player.getPlayer().getLayoutY() < 205 || Player.getPlayer().getRadiusY() == 17 && activeObstacle.getHeight() == 28) {
+                            System.out.println("geschafft");
+                        } else {
+                                checkDamage();
+                                damage = true;
+                        }
+                    }
+
+
+                    //Um die runde Form des Charakters zu imitieren, anstatt eine viereckige Hitbox
+                    if (activeObstacle.getX() < 30 && activeObstacle.getX() > 20 && !beaten && !damage) {
+                        if (Player.getPlayer().getLayoutY() < 207 || Player.getPlayer().getRadiusY() == 17 && activeObstacle.getHeight() == 28) {
+                            System.out.println("geschafft");
+                        } else {
+                                checkDamage();
+                                damage = true;
+
+                        }
+                    }
+
+            }
+
+            if (beaten) {
+                Label l = (Label) ap.getChildren().get(6);
+                Button restart = (Button) ap.getChildren().get(7);
+                Button back = (Button) ap.getChildren().get(8);
+                l.setVisible(true);
+                restart.setVisible(true);
+                back.setVisible(true);
+                enemies.stop();
+                playerJump.stop();
+                s.stop();
+                stopGame();
+            }
+
+
+            //Width + Layout + Höhe = Position
+            //36 + 14 + 72 = Ende
+            // 36 + 120 + 72 = Anfang
+            activeObstacle.setX(activeObstacle.getX() - difficulty);
+        }
+
+    };
+
+    public ObstacleGenerator(AnchorPane ap, int time, AnimationTimer playerJump) {
+        this.ap = ap;
+        this.time = time;
+        this.playerJump = playerJump;
+    }
+
+    public void checkDamage(){
+
+            for(int i = 0; i < hearts.length; i++) {
+                if (hearts[i].getFill() == Color.RED) {
+                    hearts[i].setFill(Color.WHITE);
+                    i = hearts.length;
+                }
+            }
+            if(hearts[2].getFill() == Color.WHITE){
+                beaten = true;
+            }
+    }
+
+    public void generateObstacle() {
+        Rectangle obstacle = new Rectangle();
+        int random = getRandomNumber(0, 2);
+        System.out.println(random);
+        if (random == 0) {
+            obstacle = new Rectangle(36, big);
+        }
+
+        if (random == 1) {
+            obstacle = new Rectangle(36, small);
+            obstacle.setY(251);
+        }
+        obstacle.setY(y);
+        obstacle.setX(x);
+        obstacle.setFill(Color.RED);
+        activeObstacle = obstacle;
+        ap.getChildren().add(obstacle);
+
+        enemies.start();
+    }
+
+    public void stopObstacle() {
+        enemies.stop();
+        ap.getChildren().remove(activeObstacle);
+        damage = false;
+    }
+
+    public static void setHearts(Circle[] heartcollection){
+        hearts = heartcollection;
+    }
+
+    public void stopGame(){
+        stop = true;
+    }
+
+
+    public void startGame(){
+        playerJump.start();
+        stopObstacle();
+        stop = false;
+        beaten = false;
+        synchronized (this){
+            notify();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            if (!beaten) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        generateObstacle();
+                    }
+                });
+                try {
+                    sleep(time);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if(!beaten) {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            stopObstacle();
+                        }
+                    });
+                }
+            }
+            if(stop) {
+                synchronized (this) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        }
+
+
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+}
+
--- src/main/java/com/example/Controller/HelloController.java
+++ src/main/java/com/example/Controller/HelloController.java
@@ -1,17 +1,53 @@
 package com.example.Controller;
 
+import Model.ObstacleGenerator;
+import Model.Player;
 import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private AnchorPane scene;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Ellipse player;
+
+    @FXML
+    private Label gameOver;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Button restart;
+
+    @FXML
+    private Circle heart3;
+
+    @FXML
+    private Circle heart2;
+
+    @FXML
+    private Circle heart1;
+
+    @FXML
+    private Label sc;
+
+    @FXML
+    private Label highscore;
+
+    Circle[] hearts;
+    
 
     AnimationTimer up = new AnimationTimer() {
         @Override
@@ -44,4 +80,98 @@ public class HelloController {
 
 
     };
+
+    ObstacleGenerator og;
+    ObstacleGenerator og2;
+    ObstacleGenerator og3;
+
+    Thread t;
+    Thread t1;
+    Thread t2;
+    Thread t3;
+
+
+    int pause = 2000;
+    int pause2 = 3000;
+    int pause3 = 3500;
+
+    Score s;
+    Player p;
+    public void initialize() throws InterruptedException {
+
+        Player.setPlayer(player);
+        highscore.setText("No Highscore yet");
+
+        p = new Player("Mekina2");
+
+        s = new Score(sc, highscore,p);
+
+        heart1.setFill(Color.RED);
+        heart2.setFill(Color.RED);
+        heart3.setFill(Color.RED);
+
+        hearts = new Circle[3];
+        hearts[0] = heart1;
+        hearts[1] = heart2;
+        hearts[2] = heart3;
+
+
+        ObstacleGenerator.setHearts(hearts);
+
+        gameOver.setText("Game Over");
+        back.setText("Back to Menue");
+        restart.setText("Restart Game");
+
+        gameOver.setVisible(false);
+        back.setVisible(false);
+        restart.setVisible(false);
+
+        up.start();
+
+
+        og = new ObstacleGenerator(scene,pause,up);
+        og2 = new ObstacleGenerator(scene,pause2,up);
+        og3 = new ObstacleGenerator(scene,pause3,up);
+
+        t = new Thread(og);
+        t1 = new Thread(og2);
+        t2 = new Thread(og3);
+        t3 = new Thread(s);
+
+        t.join();
+        t1.join();
+        t2.join();
+        t3.join();
+
+        t.start();
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+
+    @FXML
+    void restartGame(ActionEvent event) throws InterruptedException, IOException {
+
+        gameOver.setVisible(false);
+        back.setVisible(false);
+        restart.setVisible(false);
+
+        s.checkScores();
+        s.start();
+
+        highscore.setText(s.getHighscore());
+
+        hearts[0].setFill(Color.RED);
+        hearts[1].setFill(Color.RED);
+        hearts[2].setFill(Color.RED);
+
+        og.startGame();
+        og2.startGame();
+        og3.startGame();
+
+
+
+    }
+
 }

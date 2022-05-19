b9265f211a17e2952cba79660787d7b470a09668
nmekina <95867724+nmekina@users.noreply.github.com>
2022-05-19 14:51:45 +0200

    Arbeitsbericht, username.fxml

--- nmekina_Arbeitsbericht.md
+++ nmekina_Arbeitsbericht.md
@@ -2,3 +2,4 @@
 |-----|------------|---------|-------------------|-----------------------|-------------|--------|
 | 1   | 05.05.2022 | 1.0     | Trello            | Aufgaben besprechen, Board auf Trello angefangen | Mekina Nico | iB |
 | 2   | 06.05.2022 | 1.1     | Trello, IntelliJ  | Trello fertiggestellt, fxml Dateien mit zugehörigen Controller erstellt, Settingsmenü gestaltet und fertiggestellt | Mekina Nico | iB |
+| 3   | 19.05.2022 | 1.2     | Trello, IntelliJ  | Trello bearbeitet, startmenue und username bei beiden fxml mit Controller bearbeitet | Mekina Nico | iB |
--- src/main/java/com/example/Controller/ControllerUsername.java
+++ src/main/java/com/example/Controller/ControllerUsername.java
@@ -1,4 +1,26 @@
 package com.example.Controller;
 
+import Model.ChangeScene;
+import Model.Player;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class ControllerUsername {
+
+    @FXML
+    private TextField txt_username;
+
+    @FXML
+    private Button btn_ok;
+
+    @FXML
+    void pressed_btn_ok(ActionEvent event) throws IOException {
+        Player player = new Player(txt_username.getText());
+        ChangeScene.change_scene("spielfeld", btn_ok);
+    }
+
 }

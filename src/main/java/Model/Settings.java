c73e8f68e58a74e4476e81b2618e5a955f9374f1
nmekina <95867724+nmekina@users.noreply.github.com>
2022-06-02 15:06:02 +0200

    Credits erstellt, Settings optimiert

---
+++ src/main/java/Model/Settings.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Settings {
+    private boolean highscoreonoff;
+    private boolean musiconoff;
+    private String difficulty;
+    private String image;
+    private String musicplaying;
+}
--- src/main/java/com/example/Controller/ControllerSettingsMenue.java
+++ src/main/java/com/example/Controller/ControllerSettingsMenue.java
@@ -1,4 +1,55 @@
 package com.example.Controller;
 
+import Model.ChangeScene;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+
+import java.io.IOException;
+
 public class ControllerSettingsMenue {
+
+    @FXML
+    private Button btn_save;
+
+    @FXML
+    private RadioButton choice_highscore_on;
+
+    @FXML
+    private RadioButton choice_highscore_off;
+
+    @FXML
+    private RadioButton choice_music_on;
+
+    @FXML
+    private RadioButton choice_music_off;
+
+    @FXML
+    private ChoiceBox<?> choice_difficulty;
+
+    @FXML
+    private ChoiceBox<?> choice_image;
+
+    @FXML
+    private ChoiceBox<?> choice_music;
+
+    @FXML
+    private Button btn_credits;
+
+    @FXML
+    void btn_credits_press(ActionEvent event) throws IOException {
+        //alles speichern
+
+        ChangeScene.change_scene("credits", btn_save);
+    }
+
+    @FXML
+    void btn_save_press(ActionEvent event) throws IOException {
+        //alles speichernll
+
+        ChangeScene.change_scene("startmenue", btn_save);
+    }
+
 }

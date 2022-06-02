282d76ed6bc6a7e2d65bd140d931d2c02b09a72e
nmekina <95867724+nmekina@users.noreply.github.com>
2022-06-02 16:49:12 +0200

    Settings fertig

--- src/main/java/com/example/Controller/ControllerSettingsMenue.java
+++ src/main/java/com/example/Controller/ControllerSettingsMenue.java
@@ -1,11 +1,13 @@
 package com.example.Controller;
 
 import Model.ChangeScene;
+import Model.Settings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
@@ -27,29 +29,89 @@ public class ControllerSettingsMenue {
     private RadioButton choice_music_off;
 
     @FXML
-    private ChoiceBox<?> choice_difficulty;
+    private ChoiceBox<String> choice_difficulty;
 
     @FXML
-    private ChoiceBox<?> choice_image;
+    private TextField txt_image;
 
     @FXML
-    private ChoiceBox<?> choice_music;
+    private ChoiceBox<String> choice_music;
 
     @FXML
     private Button btn_credits;
 
+    Settings settings = new Settings();
+
+    public void initialize() {
+        choice_difficulty.setValue("medium");
+        choice_difficulty.getItems().add("medium");
+        choice_difficulty.getItems().add("beginner");
+        choice_difficulty.getItems().add("hard");
+        choice_difficulty.getItems().add("hardcore");
+
+        choice_music.setValue("Tetris");
+        choice_music.getItems().add("Tetris");
+        choice_music.getItems().add("Super Mario");
+        choice_music.getItems().add("Subway Surfer");
+    }
+
     @FXML
     void btn_credits_press(ActionEvent event) throws IOException {
         //alles speichern
+        settings.setDifficulty(choice_difficulty.getValue());
+        settings.setImage(txt_image.getText());
+        settings.setMusicplaying(choice_music.getValue());
+        if (choice_highscore_on.isSelected()) {
+            settings.setHighscoreonoff(true);
+        } else {
+            settings.setHighscoreonoff(false);
+        }
+        if (choice_music_on.isSelected()) {
+            settings.setMusiconoff(true);
+        } else {
+            settings.setMusiconoff(false);
+        }
 
         ChangeScene.change_scene("credits", btn_save);
     }
 
     @FXML
+    void choice_highscore_press_off(ActionEvent event) {
+        choice_highscore_on.setSelected(false);
+    }
+
+    @FXML
+    void choice_highscore_press_on(ActionEvent event) {
+        choice_highscore_off.setSelected(false);
+    }
+
+    @FXML
+    void choice_music_press_off(ActionEvent event) {
+        choice_music_on.setSelected(false);
+    }
+
+    @FXML
+    void choice_music_press_on(ActionEvent event) {
+        choice_music_off.setSelected(false);
+    }
+
+    @FXML
     void btn_save_press(ActionEvent event) throws IOException {
         //alles speichernll
+        settings.setDifficulty(choice_difficulty.getValue());
+        settings.setImage(txt_image.getText());
+        settings.setMusicplaying(choice_music.getValue());
+        if (choice_highscore_on.isSelected()) {
+            settings.setHighscoreonoff(true);
+        } else {
+            settings.setHighscoreonoff(false);
+        }
+        if (choice_music_on.isSelected()) {
+            settings.setMusiconoff(true);
+        } else {
+            settings.setMusiconoff(false);
+        }
 
         ChangeScene.change_scene("startmenue", btn_save);
     }
-
 }

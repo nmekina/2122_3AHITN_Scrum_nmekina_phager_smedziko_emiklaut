48a10041f58f71e489f7ef29891958daf4456bed
nmekina <95867724+nmekina@users.noreply.github.com>
2022-06-09 08:16:16 +0200

    Bugfix

--- src/main/java/com/example/Controller/ControllerSkinShop.java
+++ src/main/java/com/example/Controller/ControllerSkinShop.java
@@ -38,13 +38,12 @@ public class ControllerSkinShop {
     @FXML
     private Button select;
 
-    @FXML
 
-    Skin semir = new Skin("Semir", 0, new Image("C:\\Users\\Semir Medzikovic\\Documents\\HTL\\3AHITN\\SEW\\2122_3AHITN_Scrum_Mekina_Hager_Miklautsch_Medzikovic\\target\\classes\\com\\example\\Controller\\SemirMedzikovic.jpeg",200,100,false,false));
-    Skin elias = new Skin("Elias", 25, new Image("C:\\Users\\Semir Medzikovic\\Documents\\HTL\\3AHITN\\SEW\\2122_3AHITN_Scrum_Mekina_Hager_Miklautsch_Medzikovic\\target\\classes\\com\\example\\Controller\\EliasMiklautsch.jpeg",200,100,false,false));
-    Skin nico = new Skin("Nico", 50, new Image("C:\\Users\\Semir Medzikovic\\Documents\\HTL\\3AHITN\\SEW\\2122_3AHITN_Scrum_Mekina_Hager_Miklautsch_Medzikovic\\src\\main\\resources\\com\\example\\Controller\\pictures\\NicoMekina.jpeg",200,100,false,false));
-    Skin rester = new Skin("Rester", 75, new Image("C:\\Users\\Semir Medzikovic\\Documents\\HTL\\3AHITN\\SEW\\2122_3AHITN_Scrum_Mekina_Hager_Miklautsch_Medzikovic\\target\\classes\\com\\example\\Controller\\ManuelRester.jpeg",200,100,false,false));
-    Skin hager = new Skin("Hager", 100, new Image("C:\\Users\\Semir Medzikovic\\Documents\\HTL\\3AHITN\\SEW\\2122_3AHITN_Scrum_Mekina_Hager_Miklautsch_Medzikovic\\target\\classes\\com\\example\\Controller\\PhilippHager.jpeg",200,100,false,false));
+    Skin semir = new Skin("Semir", 0, new Image("SemirMedzikovic.jpeg",200,100,false,false));
+    Skin elias = new Skin("Elias", 25, new Image("EliasMiklautsch.jpeg",200,100,false,false));
+    Skin nico = new Skin("Nico", 50, new Image("NicoMekina.jpeg",200,100,false,false));
+    Skin rester = new Skin("Rester", 75, new Image("ManuelRester.jpeg",200,100,false,false));
+    Skin hager = new Skin("Hager", 100, new Image("PhilippHager.jpeg",200,100,false,false));
 
     Skin skinselect;
 

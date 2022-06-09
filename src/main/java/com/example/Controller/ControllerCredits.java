f75de9ff9aa54d412112299902e6c56636287c1c
nmekina <95867724+nmekina@users.noreply.github.com>
2022-06-09 22:47:31 +0200

    Credits

--- src/main/java/com/example/Controller/ControllerCredits.java
+++ src/main/java/com/example/Controller/ControllerCredits.java
@@ -1,4 +1,32 @@
 package com.example.Controller;
 
+import Model.ChangeScene;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import org.w3c.dom.events.MouseEvent;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.zip.DataFormatException;
+
 public class ControllerCredits {
+
+    @FXML
+    private Button btn_exit;
+
+    @FXML
+    void press_btn_exit(ActionEvent event) throws IOException, URISyntaxException {
+        URL url = new URL("https://trello.com/b/Dnd2o4mc/miklautschhagermekinamedzikovic");
+
+        Runtime runtime = Runtime.getRuntime();
+        runtime.exec("start chrome " + url);
+
+        ChangeScene.change_scene("startmenue", btn_exit);
+    }
+
+
 }

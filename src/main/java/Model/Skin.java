765dfc7ef1cf01364b001a2462446e06d3a82a22
emiklaut <95867791+emiklaut@users.noreply.github.com>
2022-06-10 09:40:04 +0200

    skins

--- src/main/java/Model/Skin.java
+++ src/main/java/Model/Skin.java
@@ -7,22 +7,35 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 import java.lang.reflect.Array;
+import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Vector;
 
 
 public class Skin {
+    public static final int NORMAL = 0;
+    public static final int LOWER = 1;
+    public static final int JUMP = 2;   // s.getImage(Skin.JUMP)
+
     String name;
     int level;
     Image picture;
-    static ListView<HBox> skins = new ListView<>();
+    static ListView<HBox> skins = new ListView<>();  // TODO: eigenartig, dass diese Referenz hier liegt
     static ArrayList<Skin> arr = new ArrayList<>();
-    Image array[] = new Image[2];
+    Image[] array = new Image[2];
+    Vector<Image> imgArray = new Vector<>();
 
     public Skin(String name, int level, Image picture) {
         this.name = name;
         this.level = level;
         this.picture = picture;
         arr.add(this);
+        imgArray.add(picture);
+        array[0] = picture;
+    }
+    public void addImageToSkin(Image picture){
+        array[1] = picture;
+        imgArray.add(picture);
     }
 
     public static ArrayList<Skin> getSkins(){

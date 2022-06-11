8aa66aa4df97b07e81f7fae29f40b98724357cc7
nmekina <95867724+nmekina@users.noreply.github.com>
2022-06-11 12:37:00 +0200

    Scoreboard

--- src/main/java/com/example/Controller/ControllerScoreBoard.java
+++ src/main/java/com/example/Controller/ControllerScoreBoard.java
@@ -1,12 +1,12 @@
 package com.example.Controller;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import Model.sortGames;
+import Model.sortHighscore;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -20,55 +20,40 @@ import java.util.*;
 public class ControllerScoreBoard {
 
     @FXML
-    private ListView<HBox> list_score;
+    private static ListView<HBox> list_score;
 
-    ArrayList<String[]> alleSpieler = new ArrayList<>();
-    ArrayList<String[]> bestenliste = new ArrayList<>();
-    int highscore = 0;
-    int indexfromhighscore;
+    sortGames sortGames = new sortGames(list_score);
+    sortHighscore sortHighsore = new sortHighscore(list_score);
 
 
     public void initialize() throws IOException {
-        addSpieler();
-        sort();
-
-        for (int i = 0; i < bestenliste.size(); i++) {
-                Label label = new Label("Name: " + bestenliste.get(i)[0] + " | highscore: " + bestenliste.get(i)[1] + " | games: " + bestenliste.get(i)[2]);
-                HBox h = new HBox();
-                h.getChildren().add(label);
-                h.setAlignment(Pos.CENTER);
-
-                list_score.getItems().add(h);
-        }
+        ActionEvent actionEvent = new ActionEvent();
+        sortbyhighscore(actionEvent);
     }
 
-    private void sort() {
-        while (alleSpieler.size() > 0) {
-            for (int i = 0; i < alleSpieler.size(); i++) {
-                if (Integer.parseInt(alleSpieler.get(i)[1]) > highscore) {
-                    highscore = Integer.parseInt(alleSpieler.get(i)[1]);
-                    indexfromhighscore = i;
-                }
-            }
-            bestenliste.add(alleSpieler.get(indexfromhighscore));
-            indexfromhighscore = 0;
-            highscore = 0;
-            alleSpieler.removeAll(bestenliste);
-        }
+    @FXML
+    void sortbygames(ActionEvent event) throws IOException {
+        sortGames.sort();
     }
 
-    private void addSpieler() throws IOException {
-        File file = new File("src/main/java/Model/scores.json");
-        String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
-        JSONArray json = new JSONArray(content);
+    @FXML
+    void sortbyhighscore(ActionEvent event) throws IOException {
+        sortHighsore.sort();
+    }
 
-        for (int z = 0; z < json.length(); z++) {
-            JSONObject getplayer = json.getJSONObject(z);
-            String[] einSpieler = new String[3];
-            einSpieler[0] = getplayer.getString("name");
-            einSpieler[1] = String.valueOf(getplayer.getInt("highscore"));
-            einSpieler[2] = String.valueOf(getplayer.getInt("games"));
-            alleSpieler.add(einSpieler);
+    @FXML
+    void sortbyname(ActionEvent event) throws InterruptedException, IOException {
+
+    }
+
+    public static void print(ArrayList<String[]> scoreboard) {
+        for (int i = 0; i < scoreboard.size(); i++) {
+            Label label = new Label("name: " + scoreboard.get(i)[0] + " | highscore: " + scoreboard.get(i)[1] + " | games: " + scoreboard.get(i)[2]);
+            HBox h = new HBox();
+            h.getChildren().add(label);
+            h.setAlignment(Pos.CENTER);
+
+            list_score.getItems().add(h);
         }
     }
 }

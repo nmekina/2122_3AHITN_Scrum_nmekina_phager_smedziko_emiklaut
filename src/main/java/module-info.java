5c99281dc5b38ae343865fd752eb5ad6072dea3a
smedziko <98097684+smedziko@users.noreply.github.com>
2022-06-23 13:32:08 +0200

    Credits

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module com.example._2122_3ahitn_scrum_mekina_hager_miklautsch_medzikovic {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.web;
     requires org.json;
     requires com.google.gson;
-    requires java.desktop;
 
 
     exports com.example.Controller;
+    opens com.example.Controller to javafx.fxml;
     exports Model;
-    opens Model to com.google.gson, javafx.fxml;
-    opens com.example.Controller to com.google.gson, javafx.fxml;
+    opens Model to com.google.gson;
 }
